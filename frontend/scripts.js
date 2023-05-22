f93dcc6c71f77b84076165c7325d06f7f4363534
Mihalych32 <nikita-mikhaylovsky@mail.ru>
2023-05-22 21:15:35 +0300

    feat: added event listener for submit button

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -18,3 +18,30 @@ textarea.addEventListener('keydown', function (event) {
     this.selectionStart = this.selectionEnd = start + 4;
   }
 });
+
+
+textarea.addEventListener('input', function (event) {
+  let text = this.value;
+});
+
+const submitCode = async () => {
+  const codeResponse = await fetch(`${process.env.BACKEND_URL}/api/submit/`, {
+    method: "POST",
+    body: {
+      lang_slug: "cpp",
+      content: text
+    }
+  });
+
+  if (codeResponse.status === 200) {
+    const serverAnswer = await codeResponse.json();
+    const serverResponseArea = document.getElementById('serverResponse');
+    serverResponseArea.innerHTML = serverAnswer;
+  } else {
+    const serverResponseArea = document.getElementById('serverResponse');
+    serverResponseArea = "Compilation error!"
+  }
+}
+
+let submitButton = document.querySelector('submit-button');
+submitButton = addEventListener('click', submitCode);
